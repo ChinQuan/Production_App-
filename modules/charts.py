d86b12bd337143e99ea6930289b2204ecff5ae05
ChinQuan <kamilmobile16@gmail.com>
2025-04-08 08:29:28 +0100

    Update charts.py

--- modules/charts.py
+++ modules/charts.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from modules.database import get_connection  # Importujemy połączenie z pliku database.py
+from modules.database import get_connection
 
 def show_charts():
     st.title("📈 Production Charts")
@@ -14,20 +14,19 @@ def show_charts():
         st.warning("No data available to generate charts.")
         return
 
-    # Konwersja kolumny 'date' do formatu datetime
     df['date'] = pd.to_datetime(df['date'])
 
     # Wykres trendu dziennej produkcji
     daily_production = df.groupby('date')['seal_count'].sum().reset_index()
-    fig = px.line(daily_production, x='date', y='seal_count', title='Daily Production Trend')
-    st.plotly_chart(fig)
+    fig1 = px.line(daily_production, x='date', y='seal_count', title='📅 Daily Production Trend')
+    st.plotly_chart(fig1)
 
     # Wykres produkcji na podstawie operatora
     operator_production = df.groupby('operator')['seal_count'].sum().reset_index()
-    fig = px.bar(operator_production, x='operator', y='seal_count', title='Production by Operator')
-    st.plotly_chart(fig)
+    fig2 = px.bar(operator_production, x='operator', y='seal_count', title='👨‍🏭 Production by Operator')
+    st.plotly_chart(fig2)
 
     # Wykres produkcji na podstawie firmy
     company_production = df.groupby('company')['seal_count'].sum().reset_index()
-    fig = px.pie(company_production, names='company', values='seal_count', title='Production by Company')
-    st.plotly_chart(fig)
+    fig3 = px.pie(company_production, names='company', values='seal_count', title='🏢 Production by Company')
+    st.plotly_chart(fig3)
