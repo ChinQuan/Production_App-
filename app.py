42dae998eb5b7cdd670093ea10a5f6292c869523
ChinQuan <kamilmobile16@gmail.com>
2025-04-12 19:02:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,40 @@
-
 import streamlit as st
-from modules.login import login
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
+from modules.login import login  # Import funkcji login z modułu login
 from modules.order_panel import show_order_panel
 from modules.charts import show_charts
 from modules.dashboard import show_dashboard
-from modules.user_management import show_user_panel
-from modules.database import get_orders_df
+from modules.production_analysis import calculate_average_time
+from modules.user_management import show_user_panel  # Import funkcji do zarządzania użytkownikami
 
 def main():
-    #st.sidebar.write("🧠 Debug:", st.session_state)
-
+    # Sprawdzanie, czy użytkownik jest zalogowany
     if not st.session_state.get("username"):
-        login()
+        login()  # Jeśli użytkownik nie jest zalogowany, wywołujemy funkcję login()
         return
 
+    # Gdy użytkownik jest zalogowany, wyświetlamy odpowiedni panel
     role = st.session_state.get("role", "guest")
-
+    
+    # Dodawanie różnych zakładek w zależności od roli użytkownika
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Go to", ["Order Panel", "Charts", "Dashboard", "User Management"])
 
     if page == "Order Panel":
         show_order_panel()
     elif page == "Charts":
-        df = get_orders_df()
-        show_charts(df)
+        show_charts()
     elif page == "Dashboard":
+        show_dashboard()
+    
+    elif page == "Analysis":
         df = get_orders_df()
-        show_dashboard(df)
-    elif page == "User Management" and role.lower() == "admin":
-        show_user_panel()
+        calculate_average_time(df)
+elif page == "User Management" and role == "admin":
+        show_user_panel()  # Wyświetlenie panelu do zarządzania użytkownikami tylko dla admina
     else:
         st.sidebar.warning("You don't have access to this section.")
 
