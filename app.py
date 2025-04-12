f6be6a080c027364dc824196d63dc64026e152c5
ChinQuan <kamilmobile16@gmail.com>
2025-04-12 19:03:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,23 @@ import sys
 import os
 sys.path.append(os.path.dirname(__file__))
 
-from modules.login import login  # Import funkcji login z modułu login
+from modules.login import login
 from modules.order_panel import show_order_panel
 from modules.charts import show_charts
 from modules.dashboard import show_dashboard
 from modules.production_analysis import calculate_average_time
-from modules.user_management import show_user_panel  # Import funkcji do zarządzania użytkownikami
+from modules.user_management import show_user_panel
+from modules.database import get_orders_df
 
 def main():
-    # Sprawdzanie, czy użytkownik jest zalogowany
     if not st.session_state.get("username"):
-        login()  # Jeśli użytkownik nie jest zalogowany, wywołujemy funkcję login()
+        login()
         return
 
-    # Gdy użytkownik jest zalogowany, wyświetlamy odpowiedni panel
     role = st.session_state.get("role", "guest")
-    
-    # Dodawanie różnych zakładek w zależności od roli użytkownika
+
     st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Go to", ["Order Panel", "Charts", "Dashboard", "User Management"])
+    page = st.sidebar.radio("Go to", ["Order Panel", "Charts", "Dashboard", "Analysis", "User Management"])
 
     if page == "Order Panel":
         show_order_panel()
@@ -29,14 +27,14 @@ def main():
         show_charts()
     elif page == "Dashboard":
         show_dashboard()
-    
     elif page == "Analysis":
         df = get_orders_df()
         calculate_average_time(df)
-elif page == "User Management" and role == "admin":
-        show_user_panel()  # Wyświetlenie panelu do zarządzania użytkownikami tylko dla admina
+    elif page == "User Management" and role.lower() == "admin":
+        show_user_panel()
     else:
         st.sidebar.warning("You don't have access to this section.")
 
 if __name__ == "__main__":
     main()
+
