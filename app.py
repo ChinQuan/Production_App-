c6284655ba0f9f27d8e7195e70a3bd7b6ffb6881
ChinQuan <kamilmobile16@gmail.com>
2025-04-13 10:10:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from modules.edit_orders import show_edit_orders
 import streamlit as st
 from modules.login import login
 from modules.order_panel import show_order_panel
@@ -8,9 +7,11 @@ from modules.user_management import show_user_panel
 from modules.database import get_orders_df
 from modules.analysis import calculate_average_time
 from modules.calculator import show_calculator
+from modules.edit_orders import show_edit_orders
 
 def main():
-    #st.sidebar.write("🧠 Debug:", st.session_state)
+    # Debug:
+    # st.sidebar.write("🧠 Debug:", st.session_state)
 
     if not st.session_state.get("username"):
         login()
@@ -20,30 +21,39 @@ def main():
 
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Go to", [
-        "Edit Orders","Order Panel", "Charts", "Dashboard", "User Management", "Analysis", "Calculator"])
+        "Order Panel",
+        "Charts",
+        "Dashboard",
+        "Edit Orders",
+        "User Management",
+        "Analysis",
+        "Calculator"
+    ])
 
     if page == "Order Panel":
         show_order_panel()
+
     elif page == "Charts":
         df = get_orders_df()
         show_charts(df)
+
     elif page == "Dashboard":
         df = get_orders_df()
         show_dashboard(df)
+
+    elif page == "Edit Orders":
+        df = get_orders_df()
+        show_edit_orders(df)
+
     elif page == "User Management" and role == "admin":
         show_user_panel()
-    
-elif page == "Edit Orders":
-    df = get_orders_df()
-    show_edit_orders(df)
-elif page == "Calculator":
-        df = get_orders_df()
-        show_calculator(df)
+
     elif page == "Analysis":
         df = get_orders_df()
         calculate_average_time(df)
-    else:
-        st.sidebar.warning("You don't have access to this section.")
+
+    elif page == "Calculator":
+        show_calculator()
 
 if __name__ == "__main__":
     main()
